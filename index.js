2c6de6092744e7661b20c3c128ba0aa11b3486d6
Morgan Page <me@morganpage.co.uk>
2021-02-08 19:17:35 +0000

    Color changes and tidy up

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
+
 let canvasSize = 256;
 const canvas = document.getElementById("mycanvas");
 const app = new PIXI.Application({
-  view:canvas,
+  view: canvas,
   width: canvasSize,
-  height:canvasSize,
-  backgroundColor: 0x1099bb
+  height: canvasSize,
+  backgroundColor: 0x5c812f,
 });
 
-let squareWidth = 64;
+let squareWidth = 32;
 const square = new PIXI.Sprite(PIXI.Texture.WHITE);
 square.anchor.set(0.5);
 square.position.set(app.screen.width / 2, app.screen.height / 2);
-square.width = squareWidth;
-square.height = squareWidth;
-square.tint = '0x00FF00';
+square.width = square.height = squareWidth;
+square.tint = 0xea985d;
 
 app.stage.addChild(square);
-
